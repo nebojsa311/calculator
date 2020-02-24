c1287e37fd2a9f077167ef73a693e9bd26a37637
Nebojša Draganić <nebojsa311@gmail.com>
2020-02-24 23:10:03 +0100

    First commit of js calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+    render(){
+      return(
+        <div id="calculator">
+          <div id="clear">AC</div>
+          <div id="display"></div>
+          <div id="one">1</div>
+          <div id="two">2</div>
+          <div id="add">+</div>
+          <div id="three">3</div>
+          <div id="four">4</div>
+          <div id="five">5</div>
+          <div id="six">6</div>
+          <div id="subtract">-</div>
+          <div id="seven">7</div>
+          <div id="eight">8</div>
+          <div id="nine">9</div>
+          <div id="divide">/</div>
+          <div id="zero">0</div>
+          <div id="multiply">*</div>
+          <div id="decimal">.</div>
+          <div id="equals">=</div>
+        </div>
+      )
+    }
 }
 
 export default App;
