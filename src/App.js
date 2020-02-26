edd5fa8c59b43ddb91471ff7859e7e13dff942e6
Nebojša Draganić <nebojsa311@gmail.com>
2020-02-26 21:41:56 +0100

    added CSS

--- src/App.js
+++ src/App.js
@@ -8,24 +8,24 @@ class App extends React.Component {
     render(){
       return(
         <div id="calculator">
-          <div id="clear">AC</div>
-          <div id="display"></div>
-          <div id="one">1</div>
-          <div id="two">2</div>
-          <div id="add">+</div>
-          <div id="three">3</div>
-          <div id="four">4</div>
-          <div id="five">5</div>
-          <div id="six">6</div>
-          <div id="subtract">-</div>
-          <div id="seven">7</div>
-          <div id="eight">8</div>
-          <div id="nine">9</div>
-          <div id="divide">/</div>
-          <div id="zero">0</div>
-          <div id="multiply">*</div>
-          <div id="decimal">.</div>
-          <div id="equals">=</div>
+          <div id="clear" class="buttons">AC</div>
+          <div id="display">0</div>
+          <div id="one" class="buttons">1</div>
+          <div id="two" class="buttons">2</div>
+          <div id="three" class="buttons">3</div>
+          <div id="add" class="buttons">+</div>
+          <div id="four" class="buttons">4</div>
+          <div id="five" class="buttons">5</div>
+          <div id="six" class="buttons">6</div>
+          <div id="subtract" class="buttons">-</div>
+          <div id="seven" class="buttons">7</div>
+          <div id="eight" class="buttons">8</div>
+          <div id="nine" class="buttons">9</div>
+          <div id="divide" class="buttons">/</div>
+          <div id="zero" class="buttons">0</div>
+          <div id="multiply" class="buttons">*</div>
+          <div id="decimal" class="buttons">.</div>
+          <div id="equals" class="buttons">=</div>
         </div>
       )
     }
