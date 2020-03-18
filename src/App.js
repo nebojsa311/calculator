91363d41ea9e2201f1d52a6513be6c701d978647
Nebojša Draganić <nebojsa311@gmail.com>
2020-03-18 22:54:04 +0100

    v204

--- src/App.js
+++ src/App.js
@@ -5,18 +5,51 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      zbir: 2,
+      zbir: [],
+      sabirnikPrvi: '',
+      sabirnikDrugi: '',
+      artimetika: ''
     }
 
     this.clearer = this.clearer.bind(this);
+    this.one = this.one.bind(this);
+    this.add = this.add.bind(this);
+    this.equals = this.equals.bind(this);
   }
   
   clearer(a, e=0){
     this.setState( { zbir: e } )
   }
 
+  one(){
+    let accumulator = this.state.zbir;
+    let arrayS = Array.from(accumulator);
+    arrayS.push(1);
+    this.setState( { zbir: arrayS } );  
+  }
+
+  add(){
+    let check = Array.from(this.state.zbir);
+    let tester = check.filter(element => element === "+");
+    console.log(tester);
+    if(tester.length < 1){
+      check.push("+");
+      this.setState( { zbir: check } )
+    }
+  }
+
+  equals(){
+    let result = this.state.zbir;
+    let corona = result.toString().split('+');
+    console.log(corona);
+    
+  }
+
   componentDidMount() {
     document.getElementById("clear").addEventListener('click', this.clearer );
+    document.getElementById("one").addEventListener('click', this.one);
+    document.getElementById("add").addEventListener('click', this.add);
+    document.getElementById("equals").addEventListener('click', this.equals);
   };
   
   
@@ -24,24 +57,24 @@ class App extends React.Component {
     render(){
       return(
         <div id="calculator">
-          <div id="clear" class="buttons">AC</div>
+          <div id="clear" className="buttons">AC</div>
           <div id="display">{this.state.zbir}</div>
-          <div id="one" class="buttons">1</div>
-          <div id="two" class="buttons">2</div>
-          <div id="three" class="buttons">3</div>
-          <div id="add" class="buttons">+</div>
-          <div id="four" class="buttons">4</div>
-          <div id="five" class="buttons">5</div>
-          <div id="six" class="buttons">6</div>
-          <div id="subtract" class="buttons">-</div>
-          <div id="seven" class="buttons">7</div>
-          <div id="eight" class="buttons">8</div>
-          <div id="nine" class="buttons">9</div>
-          <div id="divide" class="buttons">/</div>
-          <div id="zero" class="buttons">0</div>
-          <div id="multiply" class="buttons">*</div>
-          <div id="decimal" class="buttons">.</div>
-          <div id="equals" class="buttons">=</div>
+          <div id="one" className="buttons">1</div>
+          <div id="two" className="buttons">2</div>
+          <div id="three" className="buttons">3</div>
+          <div id="add" className="buttons">+</div>
+          <div id="four" className="buttons">4</div>
+          <div id="five" className="buttons">5</div>
+          <div id="six" className="buttons">6</div>
+          <div id="subtract" className="buttons">-</div>
+          <div id="seven" className="buttons">7</div>
+          <div id="eight" className="buttons">8</div>
+          <div id="nine" className="buttons">9</div>
+          <div id="divide" className="buttons">/</div>
+          <div id="zero" className="buttons">0</div>
+          <div id="multiply" className="buttons">*</div>
+          <div id="decimal" className="buttons">.</div>
+          <div id="equals" className="buttons">=</div>
         </div>
       )
     }
