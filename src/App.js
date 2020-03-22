efbdba5f159127c6ffc604bc4746f5e338b3f28a
Nebojša Draganić <nebojsa311@gmail.com>
2020-03-22 21:25:02 +0100

    v220

--- src/App.js
+++ src/App.js
@@ -13,43 +13,174 @@ class App extends React.Component {
 
     this.clearer = this.clearer.bind(this);
     this.one = this.one.bind(this);
+    this.two = this.two.bind(this);
+    this.three = this.three.bind(this);
+    this.four = this.four.bind(this);
+    this.five = this.five.bind(this);
+    this.six = this.six.bind(this);
+    this.seven = this.seven.bind(this);
+    this.eight = this.eight.bind(this);
+    this.nine = this.nine.bind(this);
+    this.zero = this.zero.bind(this);
     this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
     this.equals = this.equals.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
   }
-  
+
+// Reset
   clearer(a, e=0){
     this.setState( { zbir: e } )
   }
 
+// Brojevi 0-9
+
   one(){
     let accumulator = this.state.zbir;
-    let arrayS = Array.from(accumulator);
-    arrayS.push(1);
-    this.setState( { zbir: arrayS } );  
+    let onScreen = Array.from(accumulator);
+    onScreen.push(1);
+    this.setState( { zbir: onScreen } );
+     
   }
 
-  add(){
-    let check = Array.from(this.state.zbir);
-    let tester = check.filter(element => element === "+");
-    console.log(tester);
-    if(tester.length < 1){
-      check.push("+");
-      this.setState( { zbir: check } )
+  two(){
+    let accumulator = this.state.zbir;
+    let onScreen = Array.from(accumulator);
+    onScreen.push(2);
+    this.setState( { zbir: onScreen } );
+     
+  }
+
+  three(){
+    let accumulator = this.state.zbir;
+    let onScreen = Array.from(accumulator);
+    onScreen.push(3);
+    this.setState( { zbir: onScreen } );
+     
+  }
+
+  four(){
+    let accumulator = this.state.zbir;
+    let onScreen = Array.from(accumulator);
+    onScreen.push(4);
+    this.setState( { zbir: onScreen } );
+     
+  }
+
+  five(){
+    let accumulator = this.state.zbir;
+    let onScreen = Array.from(accumulator);
+    onScreen.push(5);
+    this.setState( { zbir: onScreen } );
+     
+  }
+
+  six(){
+    let accumulator = this.state.zbir;
+    let onScreen = Array.from(accumulator);
+    onScreen.push(6);
+    this.setState( { zbir: onScreen } );
+     
+  }
+
+  seven(){
+    let accumulator = this.state.zbir;
+    let onScreen = Array.from(accumulator);
+    onScreen.push(7);
+    this.setState( { zbir: onScreen } );
+     
+  }
+
+  eight(){
+    let accumulator = this.state.zbir;
+    let onScreen = Array.from(accumulator);
+    onScreen.push(8);
+    this.setState( { zbir: onScreen } );
+     
+  }
+
+  nine(){
+    let accumulator = this.state.zbir;
+    let onScreen = Array.from(accumulator);
+    onScreen.push(9);
+    this.setState( { zbir: onScreen } );
+     
+  }
+
+  zero(){
+    let accumulator = this.state.zbir;
+    let onScreen = Array.from(accumulator);
+    if(onScreen[0] === 0){
+      return false;
+    }else {
+    onScreen.push(0);
+    this.setState( { zbir: onScreen } );
     }
   }
 
-  equals(){
-    let result = this.state.zbir;
-    let corona = result.toString().split('+');
-    console.log(corona);
+  // Znak jednako
+  equals(){   
+    let cinioci = this.state.zbir.toString().split(this.state.artimetika);
+    let prvicinilac = cinioci[0].replace(/,/g, "");
+    this.setState( { sabirnikPrvi: Number(prvicinilac) } )
+    let drugicinilac = cinioci[1] === undefined  ? false : cinioci[1].replace(/,/g, "");;
+    this.setState( { sabirnikDrugi: Number(drugicinilac) } );
+    let rezultati = eval(this.state.sabirnikPrvi + this.state.artimetika + this.state.sabirnikDrugi);
+    this.setState( { zbir: Number(rezultati) } );
+   
     
+  }
+// Sabiranje, oduzimanje, množenje i djeljenje
+  add(){
+    let onScreen = Array.from(this.state.zbir);
+    if(onScreen.every(element => typeof element === "number") && onScreen.length > 0){
+      onScreen.push("+");
+      this.setState( { zbir: onScreen, artimetika: "+" } )
+    };
+  }
+
+  subtract(){
+    let onScreen = Array.from(this.state.zbir);
+    if(onScreen.every(element => typeof element === "number") && onScreen.length > 0){
+      onScreen.push("-");
+      this.setState( { zbir: onScreen, artimetika: "-" } )
+    };
+  }
+
+  multiply(){
+    let onScreen = Array.from(this.state.zbir);
+    if(onScreen.every(element => typeof element === "number") && onScreen.length > 0){
+      onScreen.push("*");
+      this.setState( { zbir: onScreen, artimetika: "*" } )
+    };
+  }
+
+  divide(){
+    let onScreen = Array.from(this.state.zbir);
+    if(onScreen.every(element => typeof element === "number") && onScreen.length > 0){
+      onScreen.push("/");
+      this.setState( { zbir: onScreen, artimetika: "/" } )
+    };
   }
 
   componentDidMount() {
     document.getElementById("clear").addEventListener('click', this.clearer );
     document.getElementById("one").addEventListener('click', this.one);
+    document.getElementById("two").addEventListener('click', this.two);
+    document.getElementById("three").addEventListener('click', this.three);
+    document.getElementById("four").addEventListener('click', this.four);
+    document.getElementById("five").addEventListener('click', this.five);
+    document.getElementById("six").addEventListener('click', this.six);
+    document.getElementById("seven").addEventListener('click', this.seven);
+    document.getElementById("eight").addEventListener('click', this.eight);
+    document.getElementById("nine").addEventListener('click', this.nine);
+    document.getElementById("zero").addEventListener('click', this.zero);
     document.getElementById("add").addEventListener('click', this.add);
     document.getElementById("equals").addEventListener('click', this.equals);
+    document.getElementById("subtract").addEventListener('click', this.subtract);
+    document.getElementById("multiply").addEventListener('click', this.multiply);
+    document.getElementById("divide").addEventListener('click', this.divide);
   };
   
   
